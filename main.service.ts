7a424a2f64ee4890b7df9718ca189d5287c77914
Shawn McKnight <smcknight@storis.com>
2024-06-25 19:41:30 -0400

    Try to make repro

---
+++ main.service.ts
@@ -0,0 +1,12 @@
+import { apple, orange, Fruits, Vegetables } from ".";
+
+const basket = {
+  Fruits: {
+    apple: Fruits.apple,
+    orange: Fruits.orange,
+  },
+  Vegetables: {
+    carrot: Vegetables.carrot,
+    cucumber: Vegetables.cucumber,
+  },
+};
---
+++ types/vegetables.ts
@@ -0,0 +1,4 @@
+export enum Vegetables {
+  carrot = "carrot",
+  cucumber = "cucumber",
+}
